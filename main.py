42d313321ceffbe765b86e2f955e646a6e026cf5
hnandan6 <hnandan6@gatech.edu>
2020-12-16 19:46:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+  "Test": "This is a test"
+}
+
+#print(programming_dictionary["Bug"])
+
+programming_dictionary["one"] = "This is the first"
+
+print(programming_dictionary)
