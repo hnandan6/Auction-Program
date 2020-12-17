caf8c5b4c10f8ed86b432d054931d903b4feaec0
hnandan6 <hnandan6@gatech.edu>
2020-12-17 03:28:07 +0000

    final

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from replit import clear
 import art
 print(art.logo)
-
+# form art import logo
+# print(logo)
 test = {}
 
 
 x = "yes"
-
+#def find_highest_bidder(bidding_record):
+#  highest_bid = 0
+#  winner = ""
+#  # bidding_record = {"Angela": 123, "James": 321}
+#  for bidder in bidding_record:
+#    bid_amount = bidding_record[bidder]
+#    if bid_amount > highest_bid: 
+#      highest_bid = bid_amount
+#      winner = bidder
+#  print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 while x != "no":
-    X1 = input("Pls Enter your Name")
-    Y1 = input("Pls Enter your bid")
+    X1 = input("Pls Enter your Name?")
+    Y1 = int(input("Pls Enter your bid? $"))
     test[X1] = Y1
     x = input("Do you want to bid - Yes or No").lower()
     if x == "yes":
@@ -19,4 +29,6 @@ while x != "no":
 #z = test[max(test, key=test.get)]
 z = max(test, key=test.get)
 #print(test)
+#find_highest_bidder(test)
+clear()
 print(f"The Winner is {z}")
